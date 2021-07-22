dd5684df3eb75217e67eae1cb73b1f3ae1762945
Colin Mitchell <muffinista@gmail.com>
2021-07-22 08:52:42 -0400

    Handle some ruby syntax issues

--- lib/enom/domain.rb
+++ lib/enom/domain.rb
@@ -11,8 +11,7 @@ module Enom
     # Second-level and first-level domain on Enom
     attr_reader :sld, :tld
 
-    # Domain expiration date (currently returns a string - 11/9/2010 11:57:39 AM)
-    attr_reader :expiration_date
+    attr_reader :response
 
     def initialize(attributes)
       #
@@ -41,8 +40,8 @@ module Enom
     # Find the domain (must be in your account) on Enom
     def self.find(name)
       sld, tld = parse_sld_and_tld(name)
-      response = Client.request("Command" => "GetDomainInfo", "SLD" => sld, "TLD" => tld)["interface_response"]["GetDomainInfo"]
-      Domain.new(response)
+      @response = Client.request("Command" => "GetDomainInfo", "SLD" => sld, "TLD" => tld)["interface_response"]["GetDomainInfo"]
+      Domain.new(@response)
     end
 
     # Determine if the domain is available for purchase
@@ -53,8 +52,8 @@ module Enom
     # Boolean helper method to determine if the domain is available for purchase
     def self.available?(name)
       sld, tld = parse_sld_and_tld(name)
-      response = Client.request("Command" => "Check", "SLD" => sld, "TLD" => tld)["interface_response"]["RRPCode"]
-      response == "210"
+      @response = Client.request("Command" => "Check", "SLD" => sld, "TLD" => tld)["interface_response"]["RRPCode"]
+      @response == "210"
     end
 
     # Determine if a list of domains are available for purchase
@@ -79,13 +78,13 @@ module Enom
         tld  = tlds
       end
 
-      response = Client.request("Command" => "Check", "SLD" => sld, "TLD" => tld, "TLDList" => list)
+      @response = Client.request("Command" => "Check", "SLD" => sld, "TLD" => tld, "TLDList" => list)
 
       result = []
-      response["interface_response"].each do |k, v|
+      @response["interface_response"].each do |k, v|
         if v == "210" #&& k[0,6] == "RRPCode"
           pos = k[7..k.size]
-          result << response["interface_response"]["Domain#{pos}"] unless k.nil? || k.empty?
+          result << @response["interface_response"]["Domain#{pos}"] unless k.nil? || k.empty?
         end
       end
 
@@ -94,10 +93,10 @@ module Enom
 
     # Find and return all domains in the account
     def self.all(options = {})
-      response = Client.request("Command" => "GetAllDomains")["interface_response"]["GetAllDomains"]["DomainDetail"]
+      @response = Client.request("Command" => "GetAllDomains")["interface_response"]["GetAllDomains"]["DomainDetail"]
 
       domains = []
-      response.each {|d| domains << Domain.new(d) }
+      @response.each {|d| domains << Domain.new(d) }
       return domains
     end
 
@@ -120,7 +119,8 @@ module Enom
       opts.merge!("NumYears" => options.delete(:years)) if options[:years]
       opts.merge!(options)
                   
-      response = Client.request({"Command" => "Purchase", "SLD" => sld, "TLD" => tld}.merge(opts))
+      @response = Client.request({"Command" => "Purchase", "SLD" => sld, "TLD" => tld}.merge(opts))
+
       Domain.find(name)
     end
 
@@ -132,8 +132,8 @@ module Enom
     def self.delete!(name, options = {})
       sld, tld = parse_sld_and_tld(name)
 
-      response = Client.request({'Command' => 'DeleteRegistration', 'SLD' => sld, 'TLD' => tld}.merge(options))['interface_response']
-      response['RRPCode'].to_i == 200
+      @response = Client.request({'Command' => 'DeleteRegistration', 'SLD' => sld, 'TLD' => tld}.merge(options))['interface_response']
+      @response['RRPCode'].to_i == 200
     end
 
     # Transfer domain from another registrar to Enom, charges the account when successful
@@ -153,9 +153,9 @@ module Enom
 
       opts.merge!("Renew" => 1) if options[:renew]
 
-      response = Client.request({"Command" => "TP_CreateOrder"}.merge(opts))["ErrCount"]
+      @response = Client.request({"Command" => "TP_CreateOrder"}.merge(opts))["ErrCount"]
 
-      response.to_i == 0
+      @response.to_i == 0
     end
 
 
@@ -164,7 +164,7 @@ module Enom
       sld, tld = parse_sld_and_tld(name)
       opts = {}
       opts.merge!("NumYears" => options[:years]) if options[:years]
-      response = Client.request({"Command" => "Extend", "SLD" => sld, "TLD" => tld}.merge(opts))
+      @response = Client.request({"Command" => "Extend", "SLD" => sld, "TLD" => tld}.merge(opts))
       Domain.find(name)
     end
 
@@ -174,10 +174,10 @@ module Enom
       sld, tld = parse_sld_and_tld(name)
       opts = {}
       opts.merge!("MaxResults" => options[:max_results] || 8, "Similar" => options[:similar] || "High")
-      response = Client.request({"Command" => "namespinner", "SLD" => sld, "TLD" => tld}.merge(opts))
+      @response = Client.request({"Command" => "namespinner", "SLD" => sld, "TLD" => tld}.merge(opts))
 
       suggestions = []
-      response["interface_response"]["namespin"]["domains"]["domain"].map do |d|
+      @response["interface_response"]["namespin"]["domains"]["domain"].map do |d|
         (options[:tlds] || %w(com net tv cc)).each do |toplevel|
           suggestions << [d["name"].downcase, toplevel].join(".") if d[toplevel] == "y"
         end
@@ -224,8 +224,8 @@ module Enom
     # Check if the domain is currently locked.  locked? helper method also available
     def locked
       unless defined?(@locked)
-        response = Client.request("Command" => "GetRegLock", "SLD" => sld, "TLD" => tld)["interface_response"]["reg_lock"]
-        @locked = response == "1"
+        @response = Client.request("Command" => "GetRegLock", "SLD" => sld, "TLD" => tld)["interface_response"]["reg_lock"]
+        @locked = @response == "1"
       end
       return @locked
     end
@@ -260,11 +260,7 @@ module Enom
     end
 
     def expiration_date
-      unless defined?(@expiration_date)
-        date_string = @domain_payload["interface_response"]["GetDomainInfo"]["status"]["expiration"]
-        @expiration_date = Date.strptime(date_string.split(" ").first, "%m/%d/%Y")
-      end
-      @expiration_date
+      @expiration_date ||= _expiration_date
     end
 
     def registration_status
@@ -299,6 +295,11 @@ module Enom
       @nameservers = attributes["services"]["entry"].first["configuration"]["dns"]
       @registration_status = attributes["status"]["registrationstatus"]
       return self
+    end
+
+    def _expiration_date
+      date_string = @domain_payload["interface_response"]["GetDomainInfo"]["status"]["expiration"]
+      Date.strptime(date_string.split(" ").first, "%m/%d/%Y")
     end
 
   end
